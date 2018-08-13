6c8a3745f5d9ba30227bde3c2866371bb55fe553
appscharles <appscharlescom@gmail.com>
2018-08-13 11:28:33 +0200

    Aller

---
+++ src/test/java/com/appscharles/libs/aller/TestCase.java
@@ -0,0 +1,14 @@
+package com.appscharles.libs.aller;
+
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * The type Test case.
+ */
+public class TestCase {
+
+    @Rule
+    public TemporaryFolder temp = new TemporaryFolder();
+
+}
